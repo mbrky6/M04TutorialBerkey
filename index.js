329ab5228015a3cc4b77b6a3ea5eef11f930bfa1
mbrky6 <mberkey6@ivytech.edu>
2023-02-12 16:08:35 -0500

    Added a server response that sends HTML code to the localhost webpage.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log("request made");
+  console.log(req.url, req.method);
+
+  res.setHeader("Content-Type", "text/html"); // Response header content type
+  res.write("<head><link rel='stylesheet' href='#'></head>")
+  res.write("<p>Request received.</p>");
+  res.write("<p style='color:#FF0000'>Now die.</p>");
+  res.write("<p>I mean, uh... Now hi.</p>")
+  res.end();
 }); // Declaring as variable/const is optional
 
 server.listen(3000, "localhost", () => {
