8b555f58aa4ef959ea16460c33a30190546b0bd0
mbrky6 <mberkey6@ivytech.edu>
2023-02-12 16:23:28 -0500

    Changed the response from a few lines of HTML code to a webpage from The Net Ninja.

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
 
   res.setHeader("Content-Type", "text/html"); // Response header content type
-  res.write("<head><link rel='stylesheet' href='#'></head>")
-  res.write("<p>Request received.</p>");
-  res.write("<p style='color:#FF0000'>Now die.</p>");
-  res.write("<p>I mean, uh... Now hi.</p>")
-  res.end();
+  
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } // if (read failed);
+    else {
+      // res.write(data);
+      res.end(data); // Can send directly through end method if only one line
+    } // else
+  });
 }); // Declaring as variable/const is optional
 
 server.listen(3000, "localhost", () => {
