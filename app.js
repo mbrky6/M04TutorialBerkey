2d3394586af7615ecac896093cf38c99e424b4e7
mbrky6 <mberkey6@ivytech.edu>
2023-02-17 18:22:55 -0500

    Added more Express functionality to allow for a 404 page. Deleted the now redundant server.js file.

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.sendFile("./views/about.html", {root:__dirname}); // Express method
 });
+
+
+// Redirects
+app.get("/about-us", (req, res) => {
+    res.redirect("/about"); // Express method
+});
+
+
+// 404 Page
+app.use((req, res) => {
+    res.status(404).sendFile("./views/404.html", {root:__dirname});
+}); // Express method set to run on every request. Because it is at the bottom of the document, it will only run if nothing above it runs first.
