7ec1211751400c0b59dd687227f3c73ac4923b8b
mbrky6 <mberkey6@ivytech.edu>
2023-02-18 21:40:09 -0500

    Replaced static HTML files with EJS from Net Ninja.

--- app.js
+++ app.js
@@ -3,26 +3,42 @@ const express = require("express");
 // Express app
 const app = express();
 
+// Register ViewEngine
+app.set("view engine", "ejs");
+// app.set("views", "folder name"); When the views folder isn't called "views"
+
 // Listen for requests
 app.listen(3000); // Localhost default
 
 app.get("/", (req, res) => {
-    // Use res.send() to send text or individual HTML lines
-    res.sendFile("./views/index.html", {root:__dirname}); // Express method
+    const blogs = [
+        {title: "Mama Mia", snippet: "I-a made a spicy-a meat-aball."},
+        {title: "Mafia", snippet: "I think the mafia is after me. I keep..."},
+        {title: "Best Pizza Recipes", snippet: "Good day, pizza lovers! Here are som..."}
+    ];
+    res.render("index", {
+        title: "Home",
+        blogs,
+    });
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile("./views/about.html", {root:__dirname}); // Express method
+    res.render("about", {
+        title: "About Us"
+    });
 });
 
 
-// Redirects
-app.get("/about-us", (req, res) => {
-    res.redirect("/about"); // Express method
+app.get("/blogs/create", (req, res) => {
+    res.render("create", {
+        title: "Create New Blog"
+    });
 });
 
 
 // 404 Page
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", {root:__dirname});
+    res.status(404).render("404", {
+        title: "Uh-oh!"
+    });
 }); // Express method set to run on every request. Because it is at the bottom of the document, it will only run if nothing above it runs first.
